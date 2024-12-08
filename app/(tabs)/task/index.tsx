eb583e32344f067a0848a6f29dc442979d3a9805
Javier Rodrigo Galian <javier.galian.1997@gmail.com>
2024-12-08 20:20:24 -0300

    add navigation with Button

--- app/(tabs)/task/index.tsx
+++ app/(tabs)/task/index.tsx
@@ -1,9 +1,22 @@
 import { Text, View, StyleSheet } from 'react-native';
+import Button from '@/components/Button';
+import { useNavigation } from 'expo-router';
+import { RootStackParamList } from '@/types/navigation';
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+
+type NavigationProps = NativeStackNavigationProp<RootStackParamList>;
 
 export default function TaskScreen() {
+
+  const navigation = useNavigation<NavigationProps>();
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Task screen</Text>
+      <Button
+        label="Crear Tarea"
+        onPress={()=>{navigation.navigate("task/CreateTask")}}
+      />
     </View>
   );
 }
